bf450182e866c7f6014598dc903f4ece465f7a25
David Leoni <noreply@davidleoni.it>
2018-11-27 18:54:13 +0100

    fixed selection sorting

--- exercises/sorting/selection_sort_solution.py
+++ exercises/sorting/selection_sort_solution.py
@@ -1,6 +1,6 @@
 def swap(A, i, j):
     """
-    In the array A, swaps the elements at position i and j.
+        In the array A, swaps the elements at position i and j.
     """
     
     #jupman-raise
@@ -18,8 +18,10 @@ def swap(A, i, j):
 
 def argmin(A, i):
     """
-    Return the index of the element in list A which is lesser or equal than all other elements in A 
-    that start from index i included
+        RETURN the *index* of the element in list A which is lesser or equal than all other elements in A 
+        that start from index i included
+
+        - MUST execute in O(n) where n is the length of A
     """
     
     #jupman-raise    
@@ -39,6 +41,8 @@ def selection_sort(A):
     Sorts the list A in-place in O(n^2) time this ways:
         1. Looks at minimal element in the array [i:n], and swaps it with first element.
         2. Repeats step 1, but considering the subarray [i+1:n]
+        
+        Remember selection sort has complexity O(n^2) where n is the size of the list.
     """
     #jupman-raise    
     
